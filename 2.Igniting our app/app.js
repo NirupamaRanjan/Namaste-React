39b8e77e099ea66d831975efe97bb35148eea2f5
Nirupama <nirupamaranjan2414@gmail.com>
2023-04-22 21:18:47 +0530

    2nd chapter

--- 2.Igniting our app/app.js
+++ 2.Igniting our app/app.js
@@ -1,1 +1,11 @@
+// In this chapter we learnt about react and react-dom package
+//Super Powers of parcel
 
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const root= document.getElementById("root")
+const heading=React.createElement("h1",{id:"heading"},"Heading created by react !!!")
+
+const rootElement=ReactDOM.createRoot(root);
+rootElement.render(heading)
